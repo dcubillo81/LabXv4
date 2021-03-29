17ad766840612dcf60d7f0451c594aebfc92c63c
Daniel Cubillo <dcubillo81@gmail.com>
2021-03-29 12:45:46 -0600

    Initial Commit

---
+++ LabXv4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LabXv4
+//
+//  Created by Daniel Cubillo on 29/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LabXv4/LabXv4App.swift
@@ -0,0 +1,17 @@
+//
+//  LabXv4App.swift
+//  LabXv4
+//
+//  Created by Daniel Cubillo on 29/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LabXv4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
